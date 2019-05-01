2a8fb1dd26e164909a808167ed04c588ab9e646f
superj80820 <superj80820@gmail.com>
2019-05-02 02:19:38 +0800

    Add db test file

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 https://gist.github.com/akosveres/b7f9173ccb7b00a8db67e5149f410bb5
 https://gist.github.com/jcavat/2ed51c6371b9b488d6a940ba1049189b
 https://ithelp.ithome.com.tw/articles/10200754?sc=pt
+[If db can't connect](https://stackoverflow.com/questions/45399941/node-js-connect-to-mysql-docker-container-econnrefused)
 
 * node
 https://ithelp.ithome.com.tw/articles/10193307
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'hihihi I am york~' });
+ var connection = mysql.createConnection({
+  host     : 'db',
+  port     : '3306',
+  user     : 'root',
+  password : 'root',
+  database : 'myDb'
+ });
+ connection.connect( function(err){
+if (err){ 
+    throw err;
+}
+else {
+console.log('Connected');
+res.render('index', { title: 'Connected'});
+}
+ });
+   
+  
 });
 
 module.exports = router;
